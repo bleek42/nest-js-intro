6c5cbcf22ba02243500b240f320db93568bb6254
bleek42 <brandonleek42@gmail.com>
2020-09-16 21:29:35 -0400

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Header('Authorizaton', 'Bearer')
   @Get()
   getHello(): string {
     return this.appService.getHello();
